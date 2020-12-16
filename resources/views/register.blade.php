c75cc82a1f9348b2ffabc7464cbd962004677416
Sahrial Hasan Wicaksana <71913572+sahrial-hasan@users.noreply.github.com>
2020-12-16 17:44:45 +0700

    Login n Register

---
+++ resources/views/register.blade.php
@@ -0,0 +1,50 @@
+<!--
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registrasi SisFo Pelayanan Publik</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link rel="stylesheet" type="text/css" href="{{ asset('asset/css/colorlib.css') }}">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('asset/images/logo.png') }}">
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
+</head>
+<body>
+	<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>Registrasi Diri</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				<form action="#" method="post">
+					<input class="text" type="text" name="Username" placeholder="Username" required="">
+					<input class="text email" type="email" name="email" placeholder="Email" required="">
+					<input class="text" type="password" name="password" placeholder="Password" required="">
+					<input class="text w3lpass" type="password" name="password" placeholder="Confirm Password" required="">
+					<div class="wthree-text">
+						<label class="anim">
+							<input type="checkbox" class="checkbox" required="">
+							<span>I Agree To The Terms & Conditions</span>
+						</label>
+						<div class="clear"> </div>
+					</div>
+					<input type="submit" value="SIGNUP">
+				</form>
+				<p>Sudah memiliki account? <a href="#"> Login!</a></p>
+			</div>
+		</div>
+
+	</div>
+	<!-- //main -->
+</body>
+</html>
