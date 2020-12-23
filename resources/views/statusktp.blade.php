775fa2ba7cf93f638bbe1fd8d4f79d6573d2da1f
mhabibjauhariid <mhabibjauhariid@gmail.com>
2020-12-23 11:46:51 +0700

    form buat ktp

--- resources/views/statusktp.blade.php
+++ resources/views/statusktp.blade.php
@@ -2,28 +2,6 @@
 @section('title', 'Status Dokumen')
 @section('konten')
 <!-- konten -->
-<h1>Status Dokumen</h1>
-<br>
-<table>
-    <thead>
-		<tr>
-			<th width="5%">No</th>
-			<th width="40%">Nama Dokumen</th>
-			<th width="30%">Status</th>
-			<th width="15%">Aksi</th>
-		</tr>
-	</thead>
-    <tbody>
-		<tr>
-		    <td><b>1</b></td>
-			<td>Perpanjang KTP</td>
-    		<td>Sedang diproses</td>
-        	<td>
-			    <a href="#" class="btn btn-primary">Edit</a>
-				<a href="#" class="btn btn-danger" onclick="return confirm('Apakah anda yakin ingin menghapus?')">Delete</a>
-	    	</td>
-		</tr>
-	</tbody>
-</table>
+<h1>Selamat Datang Di Halaman Status Dokumen</h1>
 <!-- end of konten -->
 @endsection
