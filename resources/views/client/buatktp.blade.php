3f852f82e9d8dfb4ef5a4d71ca425dbcd07d2f18
Masbr00 <jodymardika@gmail.com>
2021-01-04 11:30:21 +0700

    perbaikan tampilan user side

--- resources/views/client/buatktp.blade.php
+++ resources/views/client/buatktp.blade.php
@@ -23,13 +23,13 @@
         <div class="form-group">
             <label>Jenis Kelamin :</label><br>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="pria">
+                <input class="form-check-input" type="radio" name="p/l" id="exampleRadios2" value="laki-laki">
                 <label class="form-check-label" for="exampleRadios2">
-                    Pria
+                    Laki-Laki
                 </label>
             </div>
             <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="wanita">
+                <input class="form-check-input" type="radio" name="p/l" id="exampleRadios3" value="wanita">
                 <label class="form-check-label" for="exampleRadios3">
                     Wanita
                 </label>
@@ -37,8 +37,16 @@
         </div>
         
         <div class="form-group">
-            <label>Alamat Lengkap :</label>
-            <input type="text" class="form-control">
+            <div class="form-group border border-dark rounded px-3">
+                <label>Alamat :</label>
+                <input type="text" name="alamat" class="form-control">
+                <label>RT/RW :</label>
+                <input type="text" name="rt/rw" class="form-control">
+                <label>Kel/Desa :</label>
+                <input type="text" name="kelurahan" class="form-control">
+                <label>Kecamatan :</label>
+                <input type="text" name="kecamatan" class="form-control"><br>
+            </div>
         </div>
 
         <div class="form-group">
@@ -54,17 +62,17 @@
         </div>
 
         <div class="form-group">
-            <label>Status Perkawinan :</label>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="sudah_kawin">
+            <label>Status Perkawinan :</label><br>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="p/l" id="exampleRadios2" value="belum_kawin">
                 <label class="form-check-label" for="exampleRadios2">
-                    Sudah Kawin
+                    Belum Kawin
                 </label>
             </div>
-            <div class="form-check disabled">
-                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="belum_kawin">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="p/l" id="exampleRadios3" value="sudah_kawin">
                 <label class="form-check-label" for="exampleRadios3">
-                    Belum Kawin
+                    Sudah Kawin
                 </label>
             </div>
         </div>
@@ -72,9 +80,13 @@
         <div class="form-group">
             <label>Pekerjaan :</label>
             <select class="custom-select" id="inlineFormCustomSelectPref">
-                <option value="Pelajar">Pelajar</option>
-                <option value="PNS">PNS</option>
-                <option value="Swasta">Swasta</option>
+                <option value="belum_kerja">Belum/Tidak Bekerja</option>
+                <option value="pelajar_mahasiswa">Pelajar/Mahasiswa</option>
+                <option value="pns">Pegawai Negri Sipil</option>
+                <option value="swasta">Swasta</option>
+                <option value="karyawan_bumn">Karyawan BUMN</option>
+                <option value="buruh">Buruh</option>
+                <option value="pensiunan">Pensiunan</option>
             </select>
         </div>
 
@@ -85,6 +97,53 @@
                 <option value="wna">WNA</option>
             </select>
         </div>
+        
+        <div class="form-group">
+            <label>Upload Pas foto :</label>
+            <label>Ketentuan : </label>
+            <ol>
+                <li>Latar belakang warna biru</li>
+                <li>Ukuran 3x4</li>
+                <li>Format foto yang diperbolehkan : png, jpg, dan jpeg</li>
+            </ol>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Pas Foto</div>
+                </div>
+                <input type="file" class="form-control" name="pas_foto">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label>Upload Scan/Foto KK :</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">KK</div>
+                </div>
+                <input type="file" class="form-control" name="KK">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label>Upload Scan/Foto Surat Pengantar RT yang sudah dilegalisir :</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Surat Pengantar RT</div>
+                </div>
+                <input type="file" class="form-control" name="pengantar_rt">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label>Upload Scan/Foto Surat Pengantar Kelurahan yang sudah dilegalisir :</label>
+            <div class="input-group mb-2 mr-sm-2">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">Surat Pengantar Kelurahan</div>
+                </div>
+                <input type="file" class="form-control" name="pas_foto">
+            </div>
+        </div>
+
         <button class="btn btn-block btn-dark mb-3">Submit</button>
     </form>
 </div>
--- resources/views/client/statusktp.blade.php
+++ resources/views/client/statusktp.blade.php
@@ -8,20 +8,17 @@
     <thead class="thead-dark">
         <tr>
             <th>No</th>
+            <th>ID Dokumen</th>
             <th>Nama Dokumen</th>
             <th>Status</th>
-            <th>Aksi</th>
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td><b>1</b></td>
-            <td>Perpanjang KTP</td>
-            <td>Sedang diproses</td>
-            <td>
-                <a href="#" class="btn btn-primary">Edit</a>
-                <a href="#" class="btn btn-danger" onclick="return confirm('Apakah anda yakin ingin menghapus?')">Delete</a>
-            </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
     </tbody>
 </table>
